43736c9b0583579b3f796e3d1158a1f0588426d0
BrandonBlack-13 <95327400+BrandonBlack-13@users.noreply.github.com>
2022-03-18 19:46:06 -0500

    Added Jumper Script

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    public float speed;
+    private string PLAYER_TAG = "Player";
+
+    private bool isGrounded;
+    private string GROUND_TAG = "Ground";
+
+    [SerializeField]
+    private float jumpForce = 7f;
+
+    private Rigidbody2D myBody;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+         myBody = GetComponent<Rigidbody2D>();
+        speed = -2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myBody.velocity = new Vector2(speed, myBody.velocity.y);
+        EnemyJump();
+    }
+
+    void EnemyJump()
+    {
+       if(isGrounded)
+        {
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(PLAYER_TAG))
+        {
+
+            if (collision.collider.GetType() == typeof(UnityEngine.CircleCollider2D))
+            {
+
+                transform.position += new Vector3(1000.0f, 1000.0f, 0.0f);
+
+            }
+        }
+
+        if(collision.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded=true;
+        }
+    }
+}
